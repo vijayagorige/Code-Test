acf0c8095ea254b12a53fbde4fa5380c9874c607
vijayagorige <146085861+vijayagorige@users.noreply.github.com>
2023-12-19 12:02:12 +0530

    Update DuplicateStrings.java

--- DuplicateStrings.java
+++ DuplicateStrings.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class DuplicateStrings {
 
 	public static void main(String[] args) {
+		// declaring sacnner object
 		Scanner scanner = new Scanner(System.in);
         //entering elements length
         System.out.print("Enter the length of the array: ");
         int length = scanner.nextInt();
-
+        //array of integers
         int[] array = new int[length];
       //entering elements
         System.out.println("Enter the elements of the array");
+		//checking the condition
         for (int i = 0; i < length; i++) {
+		//printing the elements
             System.out.print("Element " + (i + 1) + ": ");
             array[i] = scanner.nextInt();
         }
@@ -23,6 +26,7 @@ public class DuplicateStrings {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] == array[j]) {
+			//printing the duplicate values
                     System.out.println(array[i]);
                     break;  // To avoid printing the same duplicate multiple times
                 }
